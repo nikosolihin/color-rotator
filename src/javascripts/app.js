5be4d5aff1227af7dbc8f1a4d0791fcf351689b0
Niko Solihin <nikosolihin@gmail.com>
2018-10-04 22:00:41 +0700

    implement funz

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,16 +1,61 @@
 /**
  * Selectors & Vars
  */
+let total = 0;
+let currentIndex = 0;
+const active = 'box--active';
 const button = document.querySelector('.button');
 const counter = document.querySelector('.counter');
-const box = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('.box');
+
+// Colors
+const css = getComputedStyle(document.body);
+const colors = [
+  css.getPropertyValue('--red'),
+  css.getPropertyValue('--blue'),
+  css.getPropertyValue('--green'),
+  css.getPropertyValue('--navy'),
+  css.getPropertyValue('--yellow'),
+  css.getPropertyValue('--orange'),
+];
 
 /**
  * Handlers
  */
-const log = e => console.log('You click me!');
+const increaseIndexAndTotal = () => {
+  currentIndex += 1;
+  currentIndex %= boxes.length;
+};
+
+const increaseTotal = () => {
+  total += 1;
+  counter.textContent = total;
+  counter.style.background = colors[total];
+  counter.style.marginTop = `${total * 35}px`;
+};
+
+const activateBox = () => {
+  const activeBox = document.querySelector(`.${active}`);
+  if (activeBox) activeBox.classList.remove(active);
+  boxes[currentIndex].classList.add(active);
+};
+
+const handleSpacebar = ({ keyCode }) => {
+  if (keyCode === 32) {
+    activateBox();
+    increaseIndexAndTotal();
+    increaseTotal();
+  }
+};
+
+const handleClick = e => {
+  activateBox();
+  increaseIndexAndTotal();
+  increaseTotal();
+};
 
 /**
  * Listeners
  */
-button.addEventListener('click', log);
+document.addEventListener('keydown', handleSpacebar);
+button.addEventListener('click', handleClick);
